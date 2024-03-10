e9b7f5a6f52504a3f45cee19ec26c9a78aedf4fd
Rahul Tevatia <rtevatia2@gmail.com>
2024-03-10 08:29:50 -0400

    Stop the ball
    In this modification, the ball will stop if it touches the bar. Else the ball will move out of reference and we have to restart the game.

--- Chapter_30.py
+++ Chapter_30.py
@@ -1,10 +1,11 @@
 """
-Chapter_30 is about working on pygames
+Chapter_30 is about working on pygames.
 Created on: 2/16/24
 Modified on:2/16/24 by Trinish Jha
-Modified on: 3/9/24 by Rahul
+Modified on: 3/10/24 by Rahul Tevatia
 """
 import pygame
+
 # Initialize pygame
 pygame.init()
 # Screen setup
@@ -13,20 +14,20 @@ length = 480
 screen = pygame.display.set_mode((width, length))
 clock = pygame.time.Clock()
 dt = 0
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
-# Setting for bar
-
+# Settings for bar
 bar_width = 70
 bar_length = 20
-# bar_x = 40
-# bar_y = 20
-# pygame.draw.rect(screen, "green", [40, 70, 90, 30]) # Homework 1 : Figure out which number is for what
-
-bar_x = (width / 2) - (bar_width / 2)
-bar_y = length - bar_length - 10
+bar_x = 385
+bar_y = 450
 bar_speed = 5
 
+# Settings for ball
+ball_radius = 13
+ball_pos_x = 200
+ball_pos_y = 70
+ball_speed = 2
+ball_moving = True
 
 running = True
 while running:
@@ -39,23 +40,25 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
-
-# Homework 2 : Make the bar moving as per user command
-
-    # pygame.draw.circle(screen, "red", player_pos, 40)
-
     keys = pygame.key.get_pressed()
-    # if keys[pygame.K_UP] and bar_x >0:
-    #     player_pos.y -= 300 * dt
-    # if keys[pygame.K_DOWN]:
-    #     player_pos.y += 300 * dt
     if keys[pygame.K_LEFT] and bar_x > 0:
         bar_x -= bar_speed
     if keys[pygame.K_RIGHT] and bar_x < width - bar_width:
         bar_x += bar_speed
 
+    if ball_moving:
+        # simple collison detection
+        if ball_pos_y + ball_radius >= bar_y and bar_x <= ball_pos_x <= bar_x + bar_width:
+            ball_moving = False
+        else:
+            ball_pos_y += ball_speed
+
+
     # Draw the basket
     pygame.draw.rect(screen, (255, 255, 255), [bar_x, bar_y, bar_width, bar_length])
+
+    # Draw the ball
+    pygame.draw.circle(screen, "red", (ball_pos_x, ball_pos_y), ball_radius)
 
     # flip() the display to put your work on screen
     pygame.display.flip()
