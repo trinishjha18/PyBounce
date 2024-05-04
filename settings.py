1178fd5f90bbb327441e8d521e0bbc8aad081f7d
Trinish Jha <trinishjha@gmail.com>
2024-05-03 21:36:15 -0400

    the ball goes back when meets the if statement but goes straight on the bar before changing directions, I think this is because of the first if statement, I am going through trials and errors.

---
+++ settings.py
@@ -0,0 +1,17 @@
+width = 640
+height = 480
+# Settings for bar
+bar_width = 70
+bar_length = 20
+bar_x = 135
+bar_y_value = 450
+bar_speed = 5
+
+# Settings for ball
+ball_radius = 13
+# ball_pos_x = random.randint(12, width - ball_radius)
+ball_pos_x = 100
+ball_pos_y = 70
+ball_speed_y = 2
+ball_speed_x = 2
+# ball_moving = True
