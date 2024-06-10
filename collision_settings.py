cd9448f6593d7ced109cb6a48677056aa0db8e5c
Trinish Jha <trinishjha@gmail.com>
2024-06-10 18:31:00 -0400

    Added a Game over label when the ball goes off the screen.

--- collision_settings.py
+++ collision_settings.py
@@ -11,7 +11,7 @@ def collision(width, height, bar_width, bar_y, bar_x, ball_pos_y, ball_speed_x, 
         ball_speed_x *= -1
     elif ball_pos_x <= ball_radius:
         ball_speed_x *= -1
-    if ball_pos_y > height + ball_radius:
+    if ball_pos_y > height + 20:
         ball_speed_x = 0
         ball_speed_y = 0
     if ball_pos_y == 430 and ball_pos_x == bar_x:
--- main_game.py
+++ main_game.py
@@ -13,7 +13,6 @@ width, height = screen_settings()
 ball_pos_y, ball_speed_x, ball_speed_y, ball_radius, ball_pos_x = ball_settings()
 bar_width, bar_length, bar_y, bar_x, bar_speed = bar_settings()
 counter = 0
-text, text_x, text_y, textx_size, texty_size, paddle, ball, font1 = play_again()
 
 # Initialize pygame
 pygame.init()
@@ -21,10 +20,11 @@ pygame.init()
 screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 dt = 0
-
+text, text_x, text_y, textx_size, texty_size, paddle, ball, font1 = play_again(width, height, bar_x, bar_y, bar_width, bar_length, screen, ball_pos_x, ball_pos_y, ball_radius)
 green, blue = colors()
 font = pygame.font.Font('freesansbold.ttf', 32)
 running = True
+game_over = False
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -32,39 +32,47 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("black")
-    # When a key is pressed move it either to the left or the right
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and bar_x > 0:
-        bar_x -= bar_speed
-    if keys[pygame.K_RIGHT] and bar_x < width - bar_width:
-        bar_x += bar_speed
+    if not game_over:
+        # fill the screen with a color to wipe away anything from last frame
+        screen.fill("black")
+        # When a key is pressed move it either to the left or the right
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and bar_x > 0:
+            bar_x -= bar_speed
+        if keys[pygame.K_RIGHT] and bar_x < width - bar_width:
+            bar_x += bar_speed
 
-    # Draw the basket
-    paddle = pygame.draw.rect(screen, (255, 255, 255), [bar_x, bar_y, bar_width, bar_length])
+        # Draw the basket
+        paddle = pygame.draw.rect(screen, (255, 255, 255), [bar_x, bar_y, bar_width, bar_length])
 
-    # Draw the ball
-    ball = pygame.draw.circle(screen, "red", (ball_pos_x, ball_pos_y), ball_radius)
-    # Start the ball movement
-    ball_pos_y += ball_speed_y
-    ball_pos_x += ball_speed_x
+        # Draw the ball
+        ball = pygame.draw.circle(screen, "red", (ball_pos_x, ball_pos_y), ball_radius)
+        # Start the ball movement
+        ball_pos_y += ball_speed_y
+        ball_pos_x += ball_speed_x
 
-    # simple collision detection
-    ball_speed_x, ball_speed_y, counter = collision(width, height, bar_width, bar_y, bar_x, ball_pos_y,
-                                                    ball_speed_x,
-                                                    ball_speed_y, ball_radius, ball_pos_x, counter)
+        # simple collision detection
+        ball_speed_x, ball_speed_y, counter = collision(width, height, bar_width, bar_y, bar_x, ball_pos_y,
+                                                        ball_speed_x,
+                                                        ball_speed_y, ball_radius, ball_pos_x, counter)
 
-    text = font.render(f"Points: {counter}", True, green, blue)
+        text = font.render(f"Points: {counter}", True, green, blue)
 
-    if ball_pos_y == 430 and ball_pos_x == bar_x:
-        x, y = event.pos
-        if text_x - 5 <= x <= text_x + textx_size + 5:
-            if text_y - 5 <= y <= text_y + texty_size + 5:
-                in_main_menu = False
-                break
+        # if ball_pos_y == 430 and ball_pos_x == bar_x:
+        #     x, y = event.pos
+        #     print(x, y)
+        #     if text_x - 5 <= x <= text_x + textx_size + 5:
+        #         if text_y - 5 <= y <= text_y + texty_size + 5:
+        #             in_main_menu = False
+        #             break
 
-    screen.blit(text, (10, 10))
+        screen.blit(text, (10, 10))
+
+    else:
+        text2 = font.render(f"GAME OVER", True, green, blue)
+        screen.blit(text2, (height/2, width/2))
+    if ball_speed_x == 0 and ball_speed_y == 0:
+        game_over = True
     pygame.display.flip()
 
     dt = clock.tick(60) / 1000
