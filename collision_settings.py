9934389cb1f6ca58ec0c7ac2acfcce2a001e6e79
Trinish Jha <trinishjha@gmail.com>
2024-06-29 19:38:35 -0400

    Brought the restart button to the center and put which level you're on

--- collision_settings.py
+++ collision_settings.py
@@ -5,15 +5,20 @@ def collision(width, height, bar_width, bar_y, bar_x, ball_pos_y, ball_speed_x, 
         ball_speed_y *= -1
         # Give the user one point
         counter += 1
+    # Top collision statement
     elif ball_pos_y <= 2:
         ball_speed_y *= -1
+    # Side collision statement 1
     if ball_pos_x >= width:
         ball_speed_x *= -1
+    # Side collision statement 2
     elif ball_pos_x <= ball_radius:
         ball_speed_x *= -1
+    # Stopping the ball when game ends
     if ball_pos_y > height + 20:
         ball_speed_x = 0
         ball_speed_y = 0
+    # Stop the side of the bar glitch
     if ball_pos_y == 430 and ball_pos_x == bar_x:
         ball_speed_x *= -1
         ball_speed_y *= -1
--- main_game.py
+++ main_game.py
@@ -9,82 +9,111 @@ from settings import *
 from collision_settings import collision
 
 
-width, height = screen_settings()
-ball_pos_y, ball_speed_x, ball_speed_y, ball_radius, ball_pos_x = ball_settings()
-bar_width, bar_length, bar_y, bar_x, bar_speed = bar_settings()
-counter = 0
+def run_game():
+    # Initialize all variables
+    width, height = screen_settings()
+    ball_pos_y, ball_speed_x, ball_speed_y, ball_radius, ball_pos_x = ball_settings()
+    bar_width, bar_length, bar_y, bar_x, bar_speed = bar_settings()
+    counter = 0
+    max_points = 0
+    level = 1
 
-# Initialize pygame
-pygame.init()
-# Screen setup
-screen = pygame.display.set_mode((width, height))
-clock = pygame.time.Clock()
-dt = 0
-text, text_x, text_y, textx_size, texty_size, paddle, ball, font1 = play_again(width, height, bar_x, bar_y, bar_width, bar_length, screen, ball_pos_x, ball_pos_y, ball_radius)
-green, blue = colors()
-font = pygame.font.Font('freesansbold.ttf', 32)
-running = True
-game_over = False
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if game_over and event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_pos = pygame.mouse.get_pos()
-            if restart_button.collidepoint(mouse_pos):
-                game_over = False
-                counter = 0
-                ball_pos_y, ball_speed_x, ball_speed_y, ball_radius, ball_pos_x = ball_settings()
-                bar_width, bar_length, bar_y, bar_x, bar_speed = bar_settings()
-    if not game_over:
-        # fill the screen with a color to wipe away anything from last frame
-        screen.fill("black")
-        # When a key is pressed move it either to the left or the right
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT] and bar_x > 0:
-            bar_x -= bar_speed
-        if keys[pygame.K_RIGHT] and bar_x < width - bar_width:
-            bar_x += bar_speed
+    # Initialize pygame
+    pygame.init()
+    # Screen setup
+    screen = pygame.display.set_mode((width, height))
+    clock = pygame.time.Clock()
+    dt = 0
+    text, text_x, text_y, textx_size, texty_size, paddle, ball, font1 = play_again(width, height, bar_x, bar_y,
+                                                                                   bar_width,
+                                                                                   bar_length, screen, ball_pos_x,
+                                                                                   ball_pos_y, ball_radius)
+    # Colors
+    green, blue = colors()
+    # Selected font
+    font = pygame.font.Font('freesansbold.ttf', 32)
+    running = True
+    game_over = False
+    while running:
+        # poll for events
+        # pygame.QUIT event means the user clicked X to close your window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if game_over and event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                if restart_button.collidepoint(mouse_pos):
+                    game_over = False
+                    counter = 0
+                    level = 1
+                    ball_pos_y, ball_speed_x, ball_speed_y, ball_radius, ball_pos_x = ball_settings()
+                    bar_width, bar_length, bar_y, bar_x, bar_speed = bar_settings()
+        if not game_over:
+            # fill the screen with a color to wipe away anything from last frame
+            screen.fill("black")
+            # When a key is pressed move it either to the left or the right
+            keys = pygame.key.get_pressed()
+            if keys[pygame.K_LEFT] and bar_x > 0:
+                bar_x -= bar_speed
+            if keys[pygame.K_RIGHT] and bar_x < width - bar_width:
+                bar_x += bar_speed
 
-        # Draw the basket
-        paddle = pygame.draw.rect(screen, (255, 255, 255), [bar_x, bar_y, bar_width, bar_length])
+            # Draw the basket
+            paddle = pygame.draw.rect(screen, (255, 255, 255), [bar_x, bar_y, bar_width, bar_length])
 
-        # Draw the ball
-        ball = pygame.draw.circle(screen, "red", (ball_pos_x, ball_pos_y), ball_radius)
-        # Start the ball movement
-        ball_pos_y += ball_speed_y
-        ball_pos_x += ball_speed_x
+            # Draw the ball
+            ball = pygame.draw.circle(screen, "red", (ball_pos_x, ball_pos_y), ball_radius)
+            # Start the ball movement
+            ball_pos_y += ball_speed_y
+            ball_pos_x += ball_speed_x
 
-        # simple collision detection
-        ball_speed_x, ball_speed_y, counter = collision(width, height, bar_width, bar_y, bar_x, ball_pos_y,
-                                                        ball_speed_x,
-                                                        ball_speed_y, ball_radius, ball_pos_x, counter)
-        if ball_speed_x == 0 and ball_speed_y == 0:
-            game_over = True
+            # Initiate simple collision detection
+            ball_speed_x, ball_speed_y, new_counter = collision(width, height, bar_width, bar_y, bar_x, ball_pos_y,
+                                                                ball_speed_x,
+                                                                ball_speed_y, ball_radius, ball_pos_x, counter)
 
-        text = font.render(f"Points: {counter}", True, green, blue)
+            # Level Up
+            if new_counter // 2 > level - 1 and new_counter > counter:
+                level += 1
+                ball_speed_x *= 1.5
+                ball_speed_y *= 1.5
+            counter = new_counter
+            # End of the game statement
+            if ball_speed_x == 0 and ball_speed_y == 0:
+                game_over = True
 
-        screen.blit(text, (10, 10))
+            # Counter Display
+            text = font.render(f"Points: {counter}", True, green)
+            screen.blit(text, (10, 10))
 
-    else:
-        text2 = font.render(f"GAME OVER! :(", True, green, blue)
-        screen.blit(text2, (height/2 - 40, width/2 - 100))
-        restart_text = font.render(f"<----Play Again?", True, green, blue)
-        screen.blit(restart_text, (350, 271))
-        restart_button = pygame.draw.rect(screen, (255, 255, 255), [200, 271, 150, 50])
+            # Max points display
+            max = font.render(f"High Score: {max_points}", True, green)
+            screen.blit(max, (10, 50))
+            if counter > max_points:
+                max_points = counter
 
-    pygame.display.flip()
+            # Level Display
+            level_display = font.render(f"Level: {level}", True, green)
+            screen.blit(level_display, (500, 10))
 
-    dt = clock.tick(60) / 1000
+        else:
+            # Once the game ends game over and a restart button will appear
+            game_over_text = font.render(f"GAME OVER! :(", True, (255, 0, 0))
+            screen.blit(game_over_text, (height / 2 - 40, width / 2 - 100))
+            restart_button = pygame.draw.rect(screen, (128, 128, 128), [240, 271, 150, 50])
+            restart_text = font.render(f"Restart", True, green)
+            screen.blit(restart_text, (255, 280))
 
-pygame.quit()
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+
+    pygame.quit()
 
 
-# def main():
-#     run_game()
-#
-#
-# if __name__ == "__main__":
-#     main()
+def main():
+    run_game()
+
+
+if __name__ == "__main__":
+    main()
